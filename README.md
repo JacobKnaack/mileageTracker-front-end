52ce7665377d12ce5fcd1b57dfcfb46a1f9032ea
Jacob Knaack <jacobknck@gmail.com>
2016-12-29 08:09:16 -0800

    updated readme, added fetch all service function and test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mileageTracker-front-end
+
+Browser code for Mileage Tracker web application by Jacob Knaack.
+
+ Built with Angular and NodeJS.
+ * requires a valid google api key to be configured as an environment variable
--- app/service/log-service.js
+++ app/service/log-service.js
@@ -32,6 +32,22 @@ function logService($log, $q, $http, $window) {
 
   service.fetchUserLogs = function(){
     $log.debug('fetching all users trip logs');
+    let config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    };
+
+    return $http.get(url, config)
+    .then(res => {
+      $log.info('fetch user logs success');
+      return $q.resolve(res.data);
+    })
+    .catch(err => {
+      $log.error('fetching all user logs failed', err.message);
+      return $q.reject(err);
+    });
   };
 
   service.deleteLog = function(){
